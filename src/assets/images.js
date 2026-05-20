0ee27297432a3f3699fcbbff013ed61fd5df1d5d
Thuat Huynh <tasteprodmusic@gmail.com>
2026-05-20 19:41:58 +0700

    them minh hoa

---
+++ src/assets/images.js
@@ -0,0 +1,15 @@
+/* Auto-generated asset URLs */
+
+export const nationalAssembly = new URL("./National_Assembly_Building_of_Vietnam.jpg", import.meta.url).href
+export const brickFactory = new URL("./Xưởng_Gạch_của_các_ông_Bourgoin_và_Meiffre_tại_Hà_Nội_(sách_Xứ_Bắc_Kỳ_của_Robert_Dubois_năm_1900).png", import.meta.url).href
+export const powerPlant1900 = new URL("./Nhà_máy_điện_thành_phố_Hà_Nội_năm_1900_-_Trung_tâm_Lưu_trữ_quốc_gia_I.jpg", import.meta.url).href
+export const hoaBinhDam = new URL("./Trên_đập_thủy_điện_Hòa_Bình_nhìn_toàn_cảnh_(3).jpg", import.meta.url).href
+export const tramSaigon1900 = new URL("./Le_Tramway_à_Saigon,_early_1900s,_showing_1m-gauge_Borsig_0-4-0T_steam_locomotive_No_5_outside_the_Compagnie_Française_de_Tramways_de_l'Indochine_Boulevard_Charner_interchange_near_the_old_Marché.jpg", import.meta.url).href
+export const metroSaigon = new URL("./tram-dung-cua-tau-dien-metro-so-1-tphcm-3.jpg", import.meta.url).href
+export const tonkinCraft = new URL("./Le_Tonkin_vers_1900_-_Kiến_Trúc_Việt-Vietnam_Architecture_10.jpg", import.meta.url).href
+export const textileFactory = new URL("./Seco-betterwork.jpg", import.meta.url).href
+export const vibeCoding = new URL("./tim-hieu-vibe-coding-la-gi.jpg", import.meta.url).href
+export const studentsAi = new URL("./IMG_5649-1024x683.jpg", import.meta.url).href
+export const vinfast = new URL("./nha-may-vinfast-hai-phong.jpg", import.meta.url).href
+export const wefJobs1 = new URL("./CleanShot 2026-05-20 at 19.27.46@2x.png", import.meta.url).href
+export const wefJobs2 = new URL("./CleanShot 2026-05-20 at 19.28.33@2x.png", import.meta.url).href
